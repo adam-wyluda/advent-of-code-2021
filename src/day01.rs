f07a81ae9ec04866fc640925311820f2831fe98b
Adam Wyłuda <adam@adamwyluda.com>
2021-12-04 11:24:21 +0100

    Days 01 & 02

---
+++ src/day01.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input-1-2.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut result = 0;
+    let mut lines = reader.lines().into_iter();
+
+    let prev_reading_0 = lines.next().unwrap()?.parse::<i32>().unwrap();
+    let mut prev_reading_1 = lines.next().unwrap()?.parse::<i32>().unwrap();
+    let mut prev_reading_2 = lines.next().unwrap()?.parse::<i32>().unwrap();
+    let mut prev_sum = prev_reading_0 + prev_reading_1 + prev_reading_2;
+
+    for line in lines {
+        let next_reading = line?.parse::<i32>().unwrap();
+        let next_sum = next_reading + prev_reading_2 + prev_reading_1;
+
+        if next_sum > prev_sum {
+            result += 1;
+        }
+
+        prev_reading_1 = prev_reading_2;
+        prev_reading_2 = next_reading;
+        prev_sum = next_sum;
+    }
+
+    println!("The result is: {}", result);
+
+    Ok(())
+}
+
+fn main_first() -> io::Result<()> {
+    let file = File::open("input-1-2.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut result = 0;
+    let mut lines = reader.lines().into_iter();
+    let mut prev_reading = lines.next().unwrap()?.parse::<i32>().unwrap();
+
+    for line in lines {
+        let next_reading = line?.parse::<i32>().unwrap();
+        if next_reading > prev_reading {
+            result += 1;
+        }
+        prev_reading = next_reading;
+    }
+
+    println!("The result is: {}", result);
+
+    Ok(())
+}
---
+++ src/day02.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input-2-2.txt")?;
+    let reader = BufReader::new(file);
+    let lines = reader.lines().into_iter();
+
+    let mut pos_x = 0;
+    let mut pos_y = 0;
+    let mut aim = 0;
+
+    for line in lines {
+        let line = line?;
+        let split = line.split(" ").collect::<Vec<&str>>();
+        let command = split[0];
+        let value = split[1].parse::<i32>().unwrap();
+
+        let (diff_x, diff_y, diff_aim) = match command {
+            "forward" => (value, value * aim, 0),
+            "up" => (0, 0, -value),
+            "down" => (0, 0, value),
+            _ => panic!("Unknown command: {}", command),
+        };
+
+        pos_x += diff_x;
+        pos_y += diff_y;
+        aim += diff_aim;
+    }
+
+    println!("The result is: {}", pos_x * pos_y);
+
+    Ok(())
+}
+
+fn main_first() -> io::Result<()> {
+    let file = File::open("input-2-2.txt")?;
+    let reader = BufReader::new(file);
+    let lines = reader.lines().into_iter();
+
+    let mut pos_x = 0;
+    let mut pos_y = 0;
+
+    for line in lines {
+        let line = line?;
+        let split = line.split(" ").collect::<Vec<&str>>();
+        let command = split[0];
+        let value = split[1].parse::<i32>().unwrap();
+
+        let (diff_x, diff_y) = match command {
+            "forward" => (value, 0),
+            "up" => (0, -value),
+            "down" => (0, value),
+            _ => panic!("Unknown command: {}", command),
+        };
+
+        pos_x += diff_x;
+        pos_y += diff_y;
+    }
+
+    println!("The result is: {}", pos_x * pos_y);
+
+    Ok(())
+}
